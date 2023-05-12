8ae7bdbbe2d38f445adf5ec1cc5ee73e4872a054
Mirko Casaccia <mirko.casaccia@external.iad2.it>
2023-05-12 14:03:21 +0200

    add test

---
+++ src/test/java/com/jenkinstest/FirstTest.java
@@ -0,0 +1,14 @@
+package com.jenkinstest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+
+	@Test
+	void test() {
+		assertEquals(1, 1);
+	}
+
+}
